a630bb927659c8435fc61d0cd4be24cac6846aea
robert-watson <robert.watson@sportsdatahub.com>
2013-09-04 15:51:54 -0700

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '3068b87815f2b67390947fae80948f920cf6cb4b4f7f29fd9f350a6baad7d6b1c40287bb038b95369ddd3e9707e77397f8c0509ec52fd6b72505bb999d2af2f8'
